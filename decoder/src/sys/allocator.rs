8dce4f3e62b03342a619705690bfadd9bd797c0e
StarsDown64 <WDNINJ4@gmail.com>
2025-02-23 23:10:31 -0500

    Init allocator

--- decoder/src/sys/allocator.rs
+++ decoder/src/sys/allocator.rs
@@ -17,12 +17,12 @@ use embedded_alloc::LlffHeap as Heap;
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
 
-// fn init_heap() {
-//     // Initialize the allocator BEFORE you use it
-//     {
-//         use core::mem::MaybeUninit;
-//         const HEAP_SIZE: usize = 1024;
-//         static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
-//         unsafe { HEAP.init(&raw mut HEAP_MEM as usize, HEAP_SIZE) }
-//     }
-// }
+pub fn init_heap() {
+    // Initialize the allocator BEFORE you use it
+    {
+        use core::mem::MaybeUninit;
+        const HEAP_SIZE: usize = 1024;
+        static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+        unsafe { HEAP.init(&raw mut HEAP_MEM as usize, HEAP_SIZE) }
+    }
+}
