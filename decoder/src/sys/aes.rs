6e9c0c68a9b3a287fb59097b504a9b1a79f643dc
Spencer <bowles7@purdue.edu>
2025-02-26 16:51:03 -0500

    Remove non-hal aes backend

--- decoder/src/sys/aes.rs
+++ decoder/src/sys/aes.rs
@@ -1,9 +1,17 @@
 //! AES Support for MAX78000
 
-pub extern crate max7800x_hal as hal;
-use hal::pac;
+// pub extern crate max7800x_hal as hal;
+// use hal::{gcr::ClockForPeripheral, pac};
 
-/// AES struct for handling encryption and decryption using the AES hardware module.
-pub struct AesBackend<const KEY_SIZE: usize> {
-    aes: pac::Aes, // Shared mutable access
-}
+// /// AES struct for handling encryption and decryption using the AES hardware module.
+// pub struct AesBackend<const KEY_SIZE: usize> {
+//     aes: pac::Aes, // Shared mutable access
+// }
+
+// impl<const KEY_SIZE: usize> AesBackend<KEY_SIZE> {
+//     fn new(aes: pac::Aes, gcr: &mut pac::gcr::Gcr) -> Self {
+//         unsafe {  }
+
+//         Self { aes }
+//     }
+// }
