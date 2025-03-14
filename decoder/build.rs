41ff3835c46fd23d071622ef1830633dc716ab77
Spencer <bowles7@purdue.edu>
2025-03-14 15:08:04 -0400

    Add fledgling build script

--- decoder/build.rs
+++ decoder/build.rs
@@ -10,10 +10,31 @@
 //!
 //! The build script also sets the linker flags to tell it which link script to use.
 
+#[path = "src/sys/mod.rs"]
+mod secure_memory;
+
+use sys::secure_memory::{Secret, SecretType};
+use std::default;
 use std::env;
+use std::convert::TryInto;
+use std::fs;
 use std::fs::File;
 use std::io::Write;
+use std::os::raw;
 use std::path::PathBuf;
+use std::path::Path;
+use serde::Deserialize;
+use serde_json;
+use serde_json::Map;
+use serde_json::Value;
+
+// include!("src/sys/secure_memory.rs");
+
+struct RawSecret {
+    id: String,
+    aes_key: Vec<u8>,
+    iv: Vec<u8>,
+}
 
 fn main() {
     // Put `memory.x` in our output directory and ensure it's
@@ -44,6 +65,74 @@ fn main() {
     //==================================//
     // Add secrets during build process //
     //==================================//
-    
+    // Read the secrets JSON file
+    let secrets_path = Path::new("../secrets.json");
+    let secrets_str = fs::read_to_string(secrets_path).expect("Failed to read secrets.json");
 
+    // Parse secrets JSON
+    let secrets: Value = serde_json::from_str(&secrets_str).expect("Invalid JSON format");
+    let secrets_map = match secrets {
+        Value::Object(map) => { map },
+        _ => { panic!("Invalid JSON format") },
+    };
+
+    let mut raw_secrets_vec = Vec::<RawSecret>::new();
+    for (id, value) in secrets_map.into_iter() {
+        let value_vec = match value {
+            Value::Array(vec) => { vec },
+            _ => { panic!("Invalid JSON format") },
+        };
+
+        let aes_key = match value_vec.get(0).unwrap() {
+            Value::String(s) => { s.to_string() },
+            _ => { panic!("Invalid JSON format") },
+        };
+
+        let iv = match value_vec.get(1).unwrap() {
+            Value::String(s) => { s.to_string() },
+            _ => { panic!("Invalid JSON format") },
+        };
+
+        let secret_arr = RawSecret {
+            id: id, 
+            aes_key: base64_url::decode(&aes_key).unwrap(),
+            iv: base64_url::decode(&iv).unwrap(),
+        };
+        raw_secrets_vec.push(secret_arr);
+    }
+
+    // Prevent overfilling flash on build
+    assert!(raw_secrets_vec.len() < 128);
+
+    let mut secrets_vec = Vec::<Secret>::new();
+    for raw_secret in raw_secrets_vec {
+        let secret_type = match raw_secret.id.as_str() {
+            "master" => { SecretType::Master }
+            _ => { SecretType::Channel(raw_secret.id.parse::<u32>().unwrap()) }
+        };
+
+        let secret = Secret {
+            secret_type: secret_type,
+            valid: true,
+            aes_key: raw_secret.aes_key,
+            aes_iv: raw_secret.iv,
+        };
+
+        secrets_vec.push(secret);
+    }
+
+    let mut secrets_arr: [Secret; 128] = [
+        Secret {
+            secret_type: SecretType::Channel(u32::MAX),
+            valid: false,
+            aes_key: [0xFFu8; 32],
+            aes_iv: [0xFFu8; 8],
+        }; 128];
+
+    for (sidx, secret) in secrets_vec.into_iter().enumerate() {
+        secrets_arr[sidx] = secret;
+    }
+
+    todo!("Decoder ID load");
+    
 }
